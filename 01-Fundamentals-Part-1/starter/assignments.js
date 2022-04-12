9e31c889b9bf00751c71f7a5a3a7274b217902d3
Sunny Sun <xiaoji.sun515@gmail.com>
2022-04-12 15:10:14 -0400

    :memo: update f1

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,5 @@
+let country = 'China', continent = 'Asia', population = '1400 millions';
+let isIsland = false;
+let language;
+
+console.log(isIsland, population, country, language);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,91 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+let myFirstJob = 'coder';
+let myCurrentJob = 'teacher';
+
+PI = 3.14159
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Sunny');
+
+javaScriptIsFun = 'YES!';
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+// a bug in JS
+console.log(typeof null);
+
+// can't mutate const variables
+// always declare using const as a best practice
+const birthYear
+
+var job = 'programmer';
+job = 'teacher';
+
+// Assignment operators
+let x = 10 + 5;
+x++; // x = x+1
+console.log(x)
+
+// Comparison operators
+const ageSarah = 10, ageJonas = 20;
+console.log(ageSarah < ageJonas);
+const isFullAge = ageSarah >= 18;
+
+// operator precedence: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence#table
+
+const firstName = 'Sunny';
+const job = 'scientist';
+const birthYear = 1991;
+
+// template string
+const sunny = `I'm ${firstName}, a ${birthYear} born ${job}!`
+console.log(sunny)
+
+console.log('String with \n\
+multiple \n\
+lines')
+
+console.log(`String with
+multiple
+lines`)
+
+const age = 15;
+
+// a controlled structure
+if (age >= 18) {
+    console.log('YES! 🎉');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`${yearsLeft} years left!`)
+}
+
+const inputYear = '1991';
+Number(inputYear); // convert to number type
+String(23);
+
+// type coercion (numbers will be converted into strings)
+console.log('I am ' + 23 + ' years old!')
+// -,*,/ converts strings into numbers
+console.log('23' - '10' - 5);
+*/
+// 5 falsy values: 0, '', undefined, null, NaN
+const age = 18;
+if (age === 18) { console.log('YES!') };
+
+18 == '18' // == does type coercion
+
+const favorite = prompt("what's your fav number!");
+console.log(favorite);
