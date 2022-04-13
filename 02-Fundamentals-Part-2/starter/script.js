83d8cbf62fcd6c05199c2d768300c600331cf442
Sunny Sun <xiaoji.sun515@gmail.com>
2022-04-12 22:01:49 -0400

    :tada: finished f2

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,85 @@
+'use strict'; // writes more secure code in strict code
+
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive!');
+
+function logger() {
+    console.log('My name is Sunny');
+}
+
+logger();
+
+function fruitProcesser(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const birthYear = 1991;
+
+// function declaration
+function calcAge(birthYear) {
+    return 2037 - birthYear;
+}
+
+// function expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+// arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const calcAge4 = (birthYear, retirementYear) => {
+
+}
+
+console.log(calcAge(birthYear), calcAge2(birthYear), calcAge3(birthYear));
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+const years = new Array(1911, 1932, 1987);
+
+friends.length
+
+// array methods
+newLength = friends.push('Jay'); // add an element to the end
+friends.unshift('John'); // add an element to the beginning
+const popped = friends.pop(); // remove the last element
+friends.shift(); // remove the first
+friends.indexOf('Steven'); // returns the index
+friends.includes('Steven'); // returns true
+
+// objects
+const friends = ['Michael', 'Steven', 'Peter'];
+const jonas = {
+    firstName: 'Jonas',
+    age: 21,
+    birthYear: 1991,
+    friends: friends,
+    calcAge: function () {
+        return 2037 - this.birthYear;
+    }
+};
+
+console.log(jonas.firstName);
+console.log(jonas['firstName']);
+
+// for loop keeps running while condition is true
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+}
+
+// continue and break
+
+// while
+let rep = 1;
+while (rep < 10) {
+    console.log(`Lifting weights repetition ${rep}`);
+    rep++;
+}
+
+*/
